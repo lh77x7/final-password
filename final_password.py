4cf2a3c2fa580b09f696c00ad0edd17bc5b65e1e
unknown <>
2020-12-01 12:26:57 +0100

    Update final_password.py

--- final_password.py
+++ final_password.py
@@ -1,9 +1,8 @@
 import random
 
-atempts = 5
-#names of heroe's == 5
-heroes = ['rocky', 'james','robin','tyler', 'micky', 'jimmy']
-secret_word = random.choice(heroes)
+lives = 9
+words = ['rocky', 'james', 'robin','tyler']
+secret_word = random.choice(words)
 clue = list('?????')
 square_symbol = u'\u2B1C'
 guessed_word_correctly = False
@@ -13,12 +12,12 @@ def update_clue(guessed_letter, secret_word, clue):
     while index < len(secret_word):
         if guessed_letter == secret_word[index]:
             clue[index] = guessed_letter
-        index += 1
+        index = index + 1
 
-while atempts > 0:
+while lives > 0:
     print(clue)
-    print('Attempts left: ' + square_symbol * atempts)
-    guess = input('Guess letter or whole word: ')
+    print('Lives left:' + square_symbol * lives)
+    guess = input('Guess a letter or whole word: ')
 
     if guess == secret_word:
         guessed_word_correctly = True
@@ -27,8 +26,8 @@ while atempts > 0:
     if guess in secret_word:
         update_clue(guess, secret_word, clue)
     else:
-        print('Incorrect. You lose an atempt')
-        atempts = atempts - 1
+        print('Incorrect. You lose a live')
+        lives = lives - 1
 
 if guessed_word_correctly:
     print('You are right! The heroes was ' + secret_word)
